4146b9371750627704816a97725d04198e2dad2e
Haylee-Govender <ST10480667@myemeris.edu.za>
2026-04-07 08:41:09 +0200

    First-Commit

---
+++ src/main/java/com/mycompany/automated_tested/Automated_Tested.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_tested;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Automated_Tested {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
